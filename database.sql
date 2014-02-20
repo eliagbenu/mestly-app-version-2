3d278cf730c6cea5e0c905fef20b0ca59e3a0f5c
Anthony Eli Agbenu <lefootballroi@yahoo.com>
2014-02-20 19:37:05 +0000

    fixed another thing..: )

--- database.sql
+++ database.sql
@@ -25,7 +25,7 @@ theme_id INT
 
 
 CREATE TABLE user_tag(
-user_id INT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+user_id INT NOT NULL users (user_id),
 tag VARCHAR(45) NOT NULL,
 CONSTRAINT chkUserTag UNIQUE(user_id,tag),
 PRIMARY KEY(user_id,tag)
